53eb2f83703c57ebe81ec1f99923c9c00fe84ecc
SergeiGorkun <sergeigorkun1992@yandex.ru>
2022-10-07 19:59:31 +0600

    Example003

---
+++ Example003/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат.
+Console.WriteLine("Введи номер четверти");
+string NumberA = Console.ReadLine();
+
+if(NumberA == "1") {
+    Console.WriteLine("Номер четверти равен единице");
+} else if(NumberA == "2"){
+    Console.WriteLine("Номер четверти равен двойке");
+} else if(NumberA == "3"){
+    Console.WriteLine("Номер четверти равен тройке");
+} else if(NumberA == "4"){
+    Console.WriteLine("Номер четверти равен четверке");
+} else {
+    Console.WriteLine("Ошибка, четверти не существует");
+}
