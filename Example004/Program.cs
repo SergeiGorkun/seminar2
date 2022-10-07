4f7b8e1a0c0c573a48f4147a61a73302d7d05c87
SergeiGorkun <sergeigorkun1992@yandex.ru>
2022-10-07 21:00:14 +0600

    Example004

---
+++ Example004/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти расстояние между точками в пространстве 2D/3D.
+Console.WriteLine("Введите координат х для первой точки");
+int NumberXA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координат y для первой точки");
+int NumberYA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координат х для второй точки");
+int NumberXB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координат y для второй точки");
+int NumberYB = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt((NumberXA - NumberXB) * (NumberXA - NumberXB) + (NumberYA - NumberYB) * (NumberYA - NumberYB));
+Console.WriteLine("Результат равен " + result);
